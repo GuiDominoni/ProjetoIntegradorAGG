bcb1e79eb5daf0105d9ccee3f574d88bb080d486
GustavoManzoni <gustavomanzoni2008@gmail.com>
2024-05-27 13:03:42 -0300

    comecei a fazer o tribunal

---
+++ ProjetoIntegrador2D/Assets/Scripts/VidaJuiz.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VidaJuiz : MonoBehaviour
+{
+    public Image healthBarFill; // Referência ao Image de preenchimento
+    public float vida, vidaMaxima, vidaMinima;
+    private void Start()
+    {
+        vidaMaxima = 100;
+        vidaMinima = 0;
+
+    }
+    private void Update()
+    {
+        if(vida > vidaMinima && vida < vidaMaxima)
+        {
+            healthBarFill.fillAmount = vida / 100;
+        
+        
+        }
+
+
+    }
+
+}
