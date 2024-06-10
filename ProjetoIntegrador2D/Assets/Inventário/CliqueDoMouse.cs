cb57e81967bf5cb9c353545d4c9806a67285ae61
GustavoManzoni <167345770+GustavoManzoni@users.noreply.github.com>
2024-06-10 10:54:07 -0300

    estou tentando fazer o inv

---
+++ ProjetoIntegrador2D/Assets/Inventário/CliqueDoMouse.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CliqueDoMouse   : MonoBehaviour
+{ 
+     public Image[] imagensParaAtivar; // Array de imagens para ativar
+
+    private void OnMouseDown()
+    {
+      
+      
+    }
+
+    
+    }
