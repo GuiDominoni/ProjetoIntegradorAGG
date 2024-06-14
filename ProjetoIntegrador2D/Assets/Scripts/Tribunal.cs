9a1b7d07ff134bebbd6e90afeafa702fb3315dfd
GustavoManzoni <167345770+GustavoManzoni@users.noreply.github.com>
2024-06-14 11:40:12 -0300

    comecei a fazer as falas no tribunal

--- ProjetoIntegrador2D/Assets/Scripts/Tribunal.cs
+++ ProjetoIntegrador2D/Assets/Scripts/Tribunal.cs
@@ -5,17 +5,85 @@ using UnityEngine;
 public class Tribunal : MonoBehaviour
 {
     public GameObject[] falas;
-    void fala1()
+    public void fala11()
     {
 
         falas[1].SetActive(false);
+        falas[2].SetActive(false);
+        falas[3].SetActive(true);
+        falas[4].SetActive(true);
 
     }
-    void fala2()
+    public void fala12()
     {
 
+        falas[1].SetActive(false);
+        falas[2].SetActive(false);
+        falas[3].SetActive(true);
+        falas[4].SetActive(true);
+
+    }
+    public void fala21()
+    {
+        falas[3].SetActive(false);
+        falas[4].SetActive(false);
+        falas[5].SetActive(true);
+        falas[6].SetActive(true);
 
 
+
+
+    }
+    public void fala22()
+    {
+        falas[3].SetActive(false);
+        falas[4].SetActive(false);
+        falas[5].SetActive(true);
+        falas[6].SetActive(true);
+
+
+
+
+    }
+    public void fala31()
+    { 
+
+        falas[5].SetActive(false);
+        falas[6].SetActive(false);
+        falas[7].SetActive(true);
+        falas[8].SetActive(true);
+
+
+
+
+    }
+    public void fala32()
+    {
+
+        falas[5].SetActive(false);
+        falas[6].SetActive(false);
+        falas[7].SetActive(true);
+        falas[8].SetActive(true);
+
+
+
+
+    }
+    public void fala41()
+    {
+        falas[7].SetActive(false);
+        falas[8].SetActive(false);
+        falas[9].SetActive(true);
+        falas[10].SetActive(true);
+
+
+    }
+    public void fala42()
+    {
+        falas[7].SetActive(false);
+        falas[8].SetActive(false);
+        falas[9].SetActive(true);
+        falas[10].SetActive(true);
 
 
     }
