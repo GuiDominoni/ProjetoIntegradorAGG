49bfaf9e8306a524bee4bb4f1f2e1ae4b55dc990
GustavoManzoni <167345770+GustavoManzoni@users.noreply.github.com>
2024-06-14 11:07:55 -0300

    so suavizei as transicoes

---
+++ ProjetoIntegrador2D/Assets/Scripts/Tribunal.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tribunal : MonoBehaviour
+{
+    public GameObject[] falas;
+    void fala1()
+    {
+
+        falas[1].SetActive(false);
+
+    }
+    void fala2()
+    {
+
+
+
+
+
+    }
+}
