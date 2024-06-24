312b72cc72952a20cf460508453c3112aa37f7f9
GustavoManzoni <167345770+GustavoManzoni@users.noreply.github.com>
2024-06-24 10:13:10 -0300

    Arrumei outro bug

--- ProjetoIntegrador2D/Assets/Niveis/Nivel3/PauseNv3.cs
+++ ProjetoIntegrador2D/Assets/Niveis/Nivel3/PauseNv3.cs
@@ -15,6 +15,34 @@ public class PauseNv3 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        inv.i11 = false;
+        inv.i12 = false;
+        inv.i13 = false;
+        inv.i14 = false;
+        inv.i21 = false;
+        inv.i22 = false;
+        inv.i23 = false;
+        inv.i24 = false;
+        inv.i31 = false;
+        inv.i32 = false;
+        inv.i33 = false;
+        inv.i34 = false;
+        inv.i41 = false;
+        inv.i42 = false;
+        inv.i43 = false;
+        inv.i44 = false;
+        inv.i51 = false;
+        inv.i52 = false;
+        inv.i53 = false;
+        inv.i54 = false;
+        inv.i61 = false;
+        inv.i62 = false;
+        inv.i63 = false;
+        inv.i64 = false;
+        inv.i71 = false;
+        inv.i72 = false;
+        inv.i73 = false;
+        inv.i74 = false;
         InvokeRepeating("timerMenos", 1, 1);
         inv.lugar = 1;
     }
