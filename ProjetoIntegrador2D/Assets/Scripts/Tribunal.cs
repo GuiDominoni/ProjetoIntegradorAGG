5a9041abfbbc09dd709b8404cb763f5271ee9e0c
GustavoManzoni <167345770+GustavoManzoni@users.noreply.github.com>
2024-07-03 13:23:27 -0300

    pode pegar

--- ProjetoIntegrador2D/Assets/Scripts/Tribunal.cs
+++ ProjetoIntegrador2D/Assets/Scripts/Tribunal.cs
@@ -13,148 +13,58 @@ public class Tribunal : MonoBehaviour
     public int cond = 0;
     public Image healthBarFill; // Referência ao Image de preenchimento
     public float vida, vidaMaxima, vidaMinima;
-    public void fala11()
-    {
-        if (inv.i21 || inv.i22 || inv.i23 || inv.i24 == true)
-        {
-            pergunta[1].SetActive(false);
-        falas[1].SetActive(false);
-        falas[2].SetActive(false);
-        falas[3].SetActive(true);
-        falas[4].SetActive(true);
-        pergunta[2].SetActive(true);
-        
-            cond++;
-
-        }
-       
-
-    }
-    public void fala12()
-    {
-        pergunta[1].SetActive(false);
-        falas[1].SetActive(false);
-        falas[2].SetActive(false);
-        falas[3].SetActive(true);
-        falas[4].SetActive(true);
-        pergunta[2].SetActive(true);
-
-    }
-    public void fala21()
-    {
-        if (inv.i11 || inv.i12 || inv.i13 || inv.i14 == true)
-        {
-            cond++;
-
-
-            pergunta[2].SetActive(false);
-            pergunta[3].SetActive(true);
-            falas[3].SetActive(false);
-            falas[4].SetActive(false);
-            falas[5].SetActive(true);
-            falas[6].SetActive(true);
-        }
-        
-      
-
-
-
-
-    }
-    public void fala22()
-    {
-        pergunta[2].SetActive(false);
-        pergunta[3].SetActive(true);
-        falas[3].SetActive(false);
-        falas[4].SetActive(false);
-        falas[5].SetActive(true);
-        falas[6].SetActive(true);
-
-
-
-
-    }
-    public void fala31()
-    {
-        if (inv.i31 || inv.i32 || inv.i33 || inv.i34 == true)
-        {
-            cond++;
-
-
-            pergunta[3].SetActive(false);
-            pergunta[4].SetActive(true);
-            falas[5].SetActive(false);
-            falas[6].SetActive(false);
-            falas[7].SetActive(true);
-            falas[8].SetActive(true);
-        }
-        
-
-
-
-
-
-    }
-    public void fala32()
-    {
-        pergunta[3].SetActive(false);
-        pergunta[4].SetActive(true);
-        falas[5].SetActive(false);
-        falas[6].SetActive(false);
-        falas[7].SetActive(true);
-        falas[8].SetActive(true);
-
-
-
-
-    }
-    public void fala41()
-    {
-        
-
-            pergunta[4].SetActive(false);
-            falas[7].SetActive(false);
-            falas[8].SetActive(false);
-        
-        if (cond >= 3)
-        {
-            venceu = true;
-
-        }else if (cond <= 2)
-        {
-            perdeu = true;
-
-        }
-
-
-    }
-    public void fala42()
-    {
-        if (inv.i61 || inv.i62 || inv.i63 || inv.i64 == true)
-        {
-            cond++;
-
-
-
-
-            pergunta[4].SetActive(false);
-            falas[7].SetActive(false);
-            falas[8].SetActive(false);
-        }
-        if (cond >= 3)
-        {
-            venceu = true;
-
-        }
-        if (cond <= 2)
-        {
-            perdeu = true;  
-
-        }
-
-
-    }
+    public bool cli1, cli2, cli3, cli4, cli5, cli6, cli7;
    
+    public void i1()
+    {
+
+        cli1 = true;
+
+
+    }
+    public void i2()
+    {
+
+        cli2 = true;
+
+
+    }
+    public void i3()
+    {
+
+
+        cli3 = true;
+
+    }
+    public void i4()
+    {
+        cli4 = true;
+
+
+
+    }
+    public void i5()
+    {
+
+
+        cli5 = true;
+
+    }
+    public void i6()
+    {
+
+        cli6 = true;
+
+
+    }
+    public void i7()
+    {
+
+        cli7 = true;
+
+
+    }
+
     public void Recomeçar()
     {
         unfade.SetActive(true);
@@ -215,6 +125,37 @@ public class Tribunal : MonoBehaviour
     }
     private void Update()
     {
+        if (pergunta[1].activeSelf && cli1 == true)
+        {
+            pergunta[1].SetActive(false);
+            pergunta[2].SetActive(true);
+            cond++;
+
+
+        }
+        if (pergunta[2].activeSelf && cli7 == true)
+        {
+
+            pergunta[2].SetActive(false);
+            pergunta[3].SetActive(true);
+            cond++;
+                
+        }
+        if (pergunta[3].activeSelf && cli4   == true)
+        {
+            pergunta[3].SetActive(false);
+            pergunta[4].SetActive(true);
+            cond++;
+
+        }
+        if (pergunta[4].activeSelf && cli6 == true)
+        {
+            cond++;
+            pergunta[4].SetActive(false);
+            
+
+        }
+        Debug.Log(cli6 + "cli6");
         vida = cond * 25;
         if (vida >= vidaMinima && vida <= vidaMaxima)
         {
