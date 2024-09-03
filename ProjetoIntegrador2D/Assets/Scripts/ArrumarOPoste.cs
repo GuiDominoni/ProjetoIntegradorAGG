96855f403d66edfefffaa54478d68625a5868f54
GustavoManzoni <167345770+GustavoManzoni@users.noreply.github.com>
2024-09-03 14:35:12 -0300

    que susto s

---
+++ ProjetoIntegrador2D/Assets/Scripts/ArrumarOPoste.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrumarOPoste : MonoBehaviour
+{
+
+    SpriteRenderer SpriteRenderer;
+    Transform Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+        Player = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player.position.y >  transform.position.y - 1.8f) 
+        { 
+            SpriteRenderer.sortingOrder = 12;
+        
+        
+        }else
+        {
+            SpriteRenderer.sortingOrder = 10;
+
+
+        }
+    }
+}
